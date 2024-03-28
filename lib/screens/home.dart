2a5fc96bb96443666c3cc27ca60516c3afa9d425
notrayo <rayoleeroy@gmail.com>
2024-03-28 19:38:55 +0300

    created ex[ese model

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+      ),
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.center,
+        //crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Home Screen'),
+        ],
       ),
     );
   }
